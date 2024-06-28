0d88d15b8345b88d486644168ebb2a3bca2b9df6
Karo <34186682+Crazyayane07@users.noreply.github.com>
2024-06-28 10:22:16 +0200

    reinit
    reinit

---
+++ Source/PaintingGame/Public/Data/PGInputData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "PGInputData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAINTINGGAME_API UPGInputData : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};

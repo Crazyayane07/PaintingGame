7df6575accc5153d7bbcaf66c2992b6c62d70e94
Karo <34186682+Crazyayane07@users.noreply.github.com>
2024-06-29 06:07:39 +0200

    Generate Mosaic
    Generate Mosaic

---
+++ Source/PaintingGame/Private/Game/PGGameManager.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Game/PGGameManager.h"
+#include "Kismet/KismetMathLibrary.h"
+
+APGGameManager::APGGameManager()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	MosaicSize = 5;
+}
+
+void APGGameManager::BeginPlay()
+{
+	Super::BeginPlay();	
+
+	GenerateMosaic();
+	StartGame();
+}
+
+void APGGameManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APGGameManager::GenerateMosaic() 
+{
+	int TrueSize = MosaicSize * MosaicSize;
+	Mosaic.Empty();
+
+	for (int i = 0; i < TrueSize; i++)
+	{
+		int randomColorId = UKismetMathLibrary::RandomInteger(AllowedColors.Num());
+		Mosaic.Add(AllowedColors[randomColorId]);
+	}
+}
+
+TArray<FLinearColor> APGGameManager::GetMosaic()
+{
+	return Mosaic;
+}
+
+int32 APGGameManager::GetMosaicSize()
+{
+	return MosaicSize;
+}
---
+++ Source/PaintingGame/Public/Game/PGGameManager.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PGGameManager.generated.h"
+
+UCLASS()
+class PAINTINGGAME_API APGGameManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	APGGameManager();
+
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly,  Category = "SetUp")
+	TArray<FLinearColor> AllowedColors;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SetUp")
+	int32 MosaicSize;
+
+	UFUNCTION(BlueprintPure, Category = "Mosaic")
+	TArray<FLinearColor> GetMosaic();
+
+	UFUNCTION(BlueprintPure, Category = "Mosaic")
+	int32 GetMosaicSize();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	void GenerateMosaic();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Game")
+	void StartGame();
+
+	TArray<FLinearColor> Mosaic;
+
+};
